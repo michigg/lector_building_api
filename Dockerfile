cc59dd07dde2a43374b0ad7ca21b1abe0a1a829a
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2020-02-04 02:16:37 +0100

    fix: dockerfile wrong config directory

--- Dockerfile
+++ Dockerfile
@@ -21,13 +21,14 @@ RUN chmod +x /start.sh
 
 WORKDIR app
 COPY src .
+COPY configs /configs
 
 RUN mkdir /run/nginx \
     && touch /run/nginx/nginx.pid \
     && chown www:www -R . /var/log/nginx /var/lib/nginx /run/nginx \
     && setcap CAP_NET_BIND_SERVICE=+eip /usr/sbin/nginx
 
-COPY ./config config
+
 USER www
 
 CMD ["/start.sh"]
--- src/config.py
+++ src/config.py
@@ -1,13 +1,8 @@
-import os
-
 API_V1_ROOT = "/api/v1"
-BUILDINGS_CONFIG_DIR = "../configs"
+BUILDINGS_CONFIG_DIR = "/configs"
 
-BUILDING_KEY_MAP = {"Erba": {'building_keys': ['WE5'], 'geojson_file': ''},
-                    "Feki": {'building_keys': ["F21", "FG1", "FG2", "FMA"], 'geojson_file': ''},
-                    "Markushaus": {'building_keys': ["M3N", "M3", "MG1", "MG2"], 'geojson_file': ''},
-                    "Innenstadt": {'building_keys': ["U2", "U5", "U7"], 'geojson_file': ''}
-                    }
-
-UNIVIS_ROOMS_API = os.environ.get("UNIVIS_ROOM_API")
-UNIVIS_ALLOCATION_API = os.environ.get("UNIVIS_ALLOCATION_API")
+# BUILDING_KEY_MAP = {"Erba": {'building_keys': ['WE5'], 'geojson_file': ''},
+#                     "Feki": {'building_keys': ["F21", "FG1", "FG2", "FMA"], 'geojson_file': ''},
+#                     "Markushaus": {'building_keys': ["M3N", "M3", "MG1", "MG2"], 'geojson_file': ''},
+#                     "Innenstadt": {'building_keys': ["U2", "U5", "U7"], 'geojson_file': ''}
+#                     }
